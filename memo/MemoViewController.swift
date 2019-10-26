6f9a53549c9730d31358ad5ed90ee35649648ac9
wakanyan <kyoto.kobe.1126.yuto@gmail.com>
2019-10-27 01:38:17 +0900

    Initial Commit

---
+++ memo/MemoViewController.swift
@@ -0,0 +1,64 @@
+//
+//  MemoViewController.swift
+//  memo
+//
+//  Created by Yuto Wakabayashi on 2019/10/26.
+//  Copyright © 2019 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController, UITextFieldDelegate {
+
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var contentsTextView: UITextView!
+    
+    var saveData: UserDefaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleTextField.text = saveData.object(forKey: "title") as? String
+        contentsTextView.text = saveData.object(forKey: "content") as? String
+        titleTextField.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func saveMemo(){
+        saveData.set(contentsTextView.text, forKey: "content")
+        saveData.set(titleTextField.text, forKey: "title")
+        
+        let alert: UIAlertController = UIAlertController(title: "保存", message: "メモの保存が完了しました。", preferredStyle: .alert)
+        
+        alert.addAction(
+            UIAlertAction(
+                title: "OK",
+                style: .default,
+                handler: { action in
+                    self.navigationController?.popViewController(animated: true)
+                }
+        )
+        )
+        present(alert, animated: true, completion: nil)
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+               textField.resignFirstResponder()
+               return true
+           }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
